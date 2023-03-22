97f7e4cbdd8e95569efcd46670464a7ad9f51ba6
Natalia Omiotek <no301713@student.polsl.pl>
2023-03-22 13:58:01 +0100

    pppp

--- MainProject/main.cpp
+++ MainProject/main.cpp
@@ -34,6 +34,29 @@ int main()
     Matrix M1{ {1, 2}, {3, 4}, {5, 6} };
     std::cout << M1.ToString() << std::endl;
 
+    //Matrix C(2, 2);
+    //Matrix B = { {1.0, 2.0}, {3.0, 4.0} };
+    //C += B;
+    //std::cout << C.ToString() << std::endl;
+    //C -= B;
+    //std::cout << C.ToString();
+
+    Matrix B = { {1.0, 2.0}, {3.0, 4.0} };
+    Matrix Beta(B);
+    B.Det();
+
+    std::cout << B.ToString() << std::endl;
+    std::cout << Beta.ToString();
+
+    //Matrix X = { {1.0, 4.0, 3.0} };
+    //Matrix Y = { {1.0, 2.0}, {4.0,2.0}, {3.0,2.0} };
+    Matrix X(5, 5);
+    Matrix Y(5, 7);
+    Matrix wynik5 = X * Y;
+
+    std::cout << wynik5.ToString();
+
+
     std::cout << "Press any key..." << std::endl;
     _getch();
 
--- MatLib-TestProject-1/test.cpp
+++ MatLib-TestProject-1/test.cpp
@@ -83,30 +83,67 @@ namespace MatrixTests {
     ));
     // Jako parametr mo¿na oczywiœcie przekazaæ równie¿ np. spodziewany wynik operacji
 
+
+
+
     TEST(Lab2, Konstruktor_Parametryczny) {
         Matrix A(2, 3, 1.0);
-
+        //brak inicjalizacji komorek, BLAD NR 1
         EXPECT_EQ(A.Rows(), 2);
         EXPECT_EQ(A.Columns(), 3);
         EXPECT_DOUBLE_EQ(A(0, 0), 1.0);
         EXPECT_DOUBLE_EQ(A(1, 2), 1.0);
+
+        Matrix B(0, 0, 0);
+        //mozliwosc stworzerzenia pustej macierzy BLAD NR 2
+       // EXPECT_DOUBLE_EQ(B(0, 0), 1.0);
+        Matrix C(1, 0);
+        //mozliwosc stworzenia macierzy 1x0????
+       // EXPECT_DOUBLE_EQ(C(0, 0), 1.0);
+        std::initializer_list<std::initializer_list<double>> lista = { {1.0, 2.0}, {3.0} };
+        Matrix D(lista);
+        EXPECT_EQ(D.Rows(), 2);
+        EXPECT_EQ(D(1, 1), 1);
+        Matrix X(0, -2);
+        //BRAK EXCEPTION THROW DLA MACIERZY (0, <0) - BLAD NR 3
+
     }
    
     class Lab_2_Param : public ::testing::TestWithParam<std::tuple<int, int, int>>
     {
     };
+
+
+
     TEST(Lab2, Konstruktor_Kopiujacy) {
-        Matrix A(7, 7);
+        std::initializer_list<std::initializer_list<double>> lista = { {1.0, 2.0}, {3.0, 4.0} };
+        Matrix A(lista);
         Matrix B(A);
-        EXPECT_EQ(A.Rows(), B.Rows()) << "Liczba wierszy nie zgadza sie";
-        EXPECT_EQ(A.Columns(), B.Columns()) << "Liczba kolumn nie zgadza sie";
-        for (int i = 0; i < 7; i++) {
-            for (int j = 0; j < 7; j++) {
-                EXPECT_EQ(A(i, j), B(i, j)) << "Elementy macierzy nie zgadzają sie";
-            }
-        }
+        EXPECT_EQ(A.Rows(), B.Rows());
+        EXPECT_EQ(A.Columns(), B.Columns());
+        EXPECT_EQ(A(0, 0), B(0, 0));
+        EXPECT_EQ(A(0, 1), B(0, 1));
+        EXPECT_EQ(A(1, 0), B(1, 0));
+        EXPECT_EQ(A(1, 1), B(1, 1));
+        Matrix C(1, 3);
+        Matrix D(C);
+        EXPECT_EQ(C.Rows(), D.Rows());
+        EXPECT_EQ(C.Columns(), D.Columns());
+        EXPECT_EQ(C(0, 0), D(0, 0));
+        EXPECT_EQ(C(0, 1), D(0, 1));
+        EXPECT_EQ(C(0, 2), D(0, 2));
+
     }
 
+    TEST(Lab2, Dodawanie_Odejmowanie_Equal) {
+        Matrix A(2, 2);
+        Matrix B = { {1.0, 2.0}, {3.0, 4.0} };
+        A += B;
+        EXPECT_TRUE(A == B);
+        A -= B; //FUNKCJA ODEJMUJACA W PRAKTYCE DODAJE - BLAD NR 4
+        Matrix C(2, 2);
+        EXPECT_TRUE(A==C);
+    }
     TEST(Lab2, Konstruktor_Kopiujacy_Wskaznik) {
         Matrix A(7, 7);
         Matrix B(A);
@@ -122,8 +159,23 @@ namespace MatrixTests {
         EXPECT_TRUE(A(1, 0) == 3.0);
         EXPECT_TRUE(A(1, 1) == 4.0);
     }
+    TEST(Lab2, Operator_Porownania) {
+        std::initializer_list<std::initializer_list<double>> lista = { {1.0, 2.0}, {3.0, 4.0} };
+        Matrix A(lista);
+        Matrix B(2, 2);
+        EXPECT_FALSE(B == A);
+        Matrix C = { {1.0, 2.0}, {3.0, 4.0} };
+        EXPECT_TRUE(A == C);
+    }
 
-    TEST_P(Lab_2_Param, Konstruktor_Kopiujacy) {
+    TEST(Lab2, Konstruktor_Inicjalizacji_Zerowy) {
+        std::initializer_list<std::initializer_list<double>> lista = { {}, {} };
+        Matrix A(lista);
+        //Tworzenie macierzy pustej z listy?
+    }
+
+
+    TEST_P(Lab_2_Param, Konstruktor_Kopiujacy_P) {
         auto const& params = GetParam();
         Matrix A(std::get<0>(params), std::get<1>(params));
         Matrix B(A);
@@ -139,15 +191,37 @@ namespace MatrixTests {
     TEST(Lab2, Operator_Przypisania) {
         Matrix A(7, 7);
         Matrix B = A;
-        EXPECT_EQ(A.Rows(), B.Rows()) << "Liczba wierszy nie zgadza sie";
-        EXPECT_EQ(A.Columns(), B.Columns()) << "Liczba kolumn nie zgadza sie";
+        
+        EXPECT_EQ(A.Rows(), B.Rows());
+        EXPECT_EQ(A.Columns(), B.Columns());
         for (int i = 0; i < 7; i++) {
             for (int j = 0; j < 7; j++) {
                 EXPECT_EQ(A(i, j), B(i, j)) << "Elementy macierzy nie zgadzają sie";
             }
         }
+        Matrix C = { {3,3} };
+        C = A;
+        EXPECT_EQ(A.Rows(), C.Rows());
+        EXPECT_EQ(A.Columns(), C.Columns());
+        for (int i = 0; i < 7; i++) {
+            for (int j = 0; j < 7; j++) {
+                EXPECT_EQ(A(i, j), C(i, j));
+            }
+        }
     }
 
+    TEST(Lab2, Operator_Nawiasow) {
+        Matrix A(2, 2);
+        for (int i = 0; i < A.Rows(); i++)
+        {
+            for (int j = 0; j < A.Columns(); j++)
+            {
+                auto val = A(i, j);
+                EXPECT_EQ(val, 0);
+            }
+            std::cout << std::endl;
+        }
+    }
     TEST(Lab2, Operator_Przypisania_Wskaznik) {
         Matrix A(7, 7);
         Matrix B(A);
@@ -192,21 +266,35 @@ namespace MatrixTests {
         EXPECT_EQ(wynik.Columns(), 2);
         EXPECT_DOUBLE_EQ(wynik(0, 0), 0.0);
         EXPECT_DOUBLE_EQ(wynik(1, 1), 3.0);
+        //NIEPOPRAWNE WYNIKI ODEJMOWANIA - BLAD 5
     }
     
     TEST(Lab2, Mnozenie) {
-        Matrix A(1, 1, 3.0);
-        Matrix B(1, 1, 7.0);
-        Matrix wynik = A * B;
-        Matrix C(A * B);
-        EXPECT_DOUBLE_EQ(C(0, 0), (3.0 * 7.0));
-        EXPECT_DOUBLE_EQ(wynik(0, 0), (3.0 * 7.0));
+  
         //mnozenie nie dziala poniewaz konstruktor parametryczny nie dziala!
         Matrix D = { {1.0, 2.0}, {3.0, 4.0} };
         Matrix E = { {1.0, 1.0}, {1.0, 1.0} };
+        Matrix Delta(D);
+        Matrix Epsilon(E);
         Matrix wynik1 = D * E;
-        EXPECT_DOUBLE_EQ(wynik1(0, 0), 3);
-        EXPECT_DOUBLE_EQ(wynik1(1, 1), 7);
+        EXPECT_DOUBLE_EQ(wynik1(0, 0), 3.0);
+        EXPECT_DOUBLE_EQ(wynik1(0, 1), 3.0);
+        EXPECT_DOUBLE_EQ(wynik1(1, 0), 7.0);
+        EXPECT_DOUBLE_EQ(wynik1(1, 1), 7.0);
+        EXPECT_TRUE(D == Delta);
+        EXPECT_TRUE(E == Epsilon);
+
+        Matrix X = { {} };
+        X.ToString(); //Macierz zerowa 1x2
+
+        Matrix F = { {1.0}, {3.0,4.0} }; //nielegalna macierz??? - BLAD NR 6
+        Matrix G = { {1.0, 1.0}, {1.0, 1.0} };
+        // Matrix wynik2 = F * G;
+        //wyrzuca dimensions mismatch 
+
+        EXPECT_EQ(F.Columns(), 2);
+        //EXPECT_DOUBLE_EQ(F(1, 1), 4.0); - NIEZGODNOSC Z TO.STRING()    BLAD NR 7
+        EXPECT_EQ(F.Rows(), 2);
 
     }
 
@@ -219,13 +307,15 @@ namespace MatrixTests {
         Matrix B(y, x, z);
         Matrix C((A * B));
         EXPECT_DOUBLE_EQ(C(0, 0), (y * z * z));
+
+
     }
 
-    TEST(Lab2, Inicjalizacja) {
-        Matrix A(1, 2, 3.0);
-        EXPECT_TRUE(A(0, 0) == 3.0);
-        EXPECT_TRUE(A(0, 1) == 3.0);
-    }
+    //TEST(Lab2, Inicjalizacja) {
+    //    Matrix A(1, 2, 3.0);
+    //    EXPECT_TRUE(A(0, 0) == 3.0);
+    //    EXPECT_TRUE(A(0, 1) == 3.0);
+    //}
 
     TEST(Lab2, Wyznacznik) {
         Matrix B(4, 4, 7.0);
@@ -233,14 +323,24 @@ namespace MatrixTests {
         std::initializer_list<std::initializer_list<double>> lista = { {1.0, 2.0}, {3.0, 4.0} };
         Matrix A(lista);
         EXPECT_EQ(A.Det(), -2);
+        std::initializer_list<std::initializer_list<double>> lista2 = { {1} };
+        Matrix C(lista2);
+        EXPECT_EQ(C.Det(), 1);
+        std::initializer_list<std::initializer_list<double>> lista3 = { {1.0, 2.0}, {3.0} };
+        Matrix D(lista3);
+        Matrix E(D);
+        EXPECT_EQ(D.Det(), 0);
+        //LICZENIE WYZNACZNIKA DLA NIEKWADRATOWYCH MACIERZY - BLAD 8
+        EXPECT_TRUE(E == D);
+        //FUNKCJA DET() NADPISUJE MACIERZ BLEDNYMI DANYMI! - BLAD 9
     }
 
-    TEST(Lab2, Destruktor) {
-        Matrix* A = new Matrix(2,2);
-        EXPECT_FALSE(A == nullptr);
-        delete A;
-        EXPECT_TRUE(A == nullptr);
-    }
+    //TEST(Lab2, Destruktor) {
+    //    Matrix* A = new Matrix(2,2);
+    //    EXPECT_FALSE(A == nullptr);
+    //    delete A;
+    //    EXPECT_TRUE(A == nullptr);
+    //}
 
 
 }
